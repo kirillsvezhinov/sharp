9622863677769591cc9aef45cbf0e1a0d5b8573c
Ильяс Такбиршин <yokep404@gmail.com>
2022-04-17 21:24:53 +0500

    Обновление #4 (Лаб. 7 API): • Добавлены следующие классы с методами (каркас): ◘ User; ◘ AuthController. • CRLF -> LF.

---
+++ шарп проект/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace шарп_проект
+{
+    class AuthController
+    {
+        public User Login(string login, string password)
+        {
+            return null;
+        }
+
+        public User Registration(string login, string password, string role)
+        {
+            return null;
+        }
+    }
+}
---
+++ шарп проект/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace шарп_проект
+{
+    class User
+    {
+        public string FullName { get; set; }
+        public int id { get; set; }
+        public string Role { get; set; }
+        public string login { get; set; }
+        public string Password { get; set; }
+    }
+}
